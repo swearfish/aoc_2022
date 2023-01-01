9509f3d4d98150415ddbb85363ee0c5d3df16801
Akos Nagy <akos.nagy.mr@gmail.com>
2023-01-01 13:42:47 +0100

    Day 01

---
+++ day01_calorie_counting/calorie_counting.py
@@ -0,0 +1,33 @@
+def read_input_file(file_name: str) -> list[int]:
+    with open(file_name) as f:
+        lines = f.readlines()
+        return read_input(lines)
+
+
+def read_input(lines: list[str]) -> list[int]:
+    total_calories = 0
+    calories_by_elf = []
+    for line in lines:
+        line = line.strip()
+        if 0 == len(line):
+            calories_by_elf.append(total_calories)
+            total_calories = 0
+        else:
+            calories = int(line)
+            total_calories += calories
+    if 0 != total_calories:
+        calories_by_elf.append(total_calories)
+    return calories_by_elf
+
+
+def find_max_calories(file_name: str) -> int:
+    input = read_input_file(file_name)
+    result = max(input)
+    return result
+
+
+def find_top_3_calories(file_name: str) -> int:
+    input = read_input_file(file_name)
+    input.sort(reverse=True)
+    result = sum(input[0:3])
+    return result
---
+++ day01_calorie_counting/test_calorie_counting.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+
+from day01_calorie_counting.calorie_counting import find_max_calories, find_top_3_calories
+
+
+class TestCalorieCounting(TestCase):
+    def test_example_max(self):
+        self.assertEqual(24000, find_max_calories('example.in'))
+
+    def test_example_top_3(self):
+        self.assertEqual(45000, find_top_3_calories('example.in'))
+
+    def test_input_max(self):
+        self.assertEqual(70374, find_max_calories('input.in'))
+
+    def test_input_top_3(self):
+        self.assertEqual(204610, find_top_3_calories('input.in'))
