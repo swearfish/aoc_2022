5bc3a587ce44365224a745b24cb322cfc47fa95e
Akos Nagy <akos.nagy.mr@gmail.com>
2023-01-01 22:59:26 +0100

    Reorganizing files

--- day04_camp_cleanup/test.py
+++ day04_camp_cleanup/test.py
@@ -3,13 +3,15 @@ from unittest import TestCase
 from day04_camp_cleanup.main import get_part_1_results_from_file, get_part_2_results_from_file
 
 
-class Test(TestCase):
+class TestPart1(TestCase):
     def test_part1_example(self):
         self.assertEqual(2, get_part_1_results_from_file('example.in'))
 
     def test_part1_input(self):
         self.assertEqual(526, get_part_1_results_from_file('input.in'))
 
+
+class TestPart2(TestCase):
     def test_part2_example(self):
         self.assertEqual(4, get_part_2_results_from_file('example.in'))
 
--- day05_supply_stacks/test.py
+++ day05_supply_stacks/test.py
@@ -3,13 +3,15 @@ from unittest import TestCase
 from day05_supply_stacks.main import get_part_1_results_from_file, get_part_2_results_from_file
 
 
-class Test(TestCase):
+class TestPart1(TestCase):
     def test_part1_example(self):
         self.assertEqual("CMZ", get_part_1_results_from_file('example.in'))
 
     def test_part1_input(self):
         self.assertEqual("SHQWSRBDL", get_part_1_results_from_file('input.in'))
 
+
+class TestPart2(TestCase):
     def test_part2_example(self):
         self.assertEqual("MCD", get_part_2_results_from_file('example.in'))
 
